f99ec614d9b6766334d6bea3a67daf8071425783
leandrocunha526 <leandrocunha016@gmail.com>
2021-04-15 18:07:45 -0300

    Import project

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { Component } from "react";
+import { StyleSheet, SafeAreaView, FlatList, ScrollView } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Produtos from "./src/components/produtos";
+
+import api from "./src/services/api";
+
+import Titulo from "./src/components/titulo";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      produtos: [],
+    };
+  }
+
+  async componentDidMount() {
+    const response = await api.get("items");
+    this.setState({
+      produtos: response.data,
+    });
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <ScrollView style={styles.scrollView}>
+          <StatusBar style="auto" />
+          <Titulo titulo="Lista de produtos"></Titulo>
+          <FlatList
+            data={this.state.produtos}
+            style={styles.list}
+            keyExtractor={(item) => item.id.toString()}
+            contentContainerStyle={styles.content}
+            renderItem={({ item }) => <Produtos data={item} />}
+          />
+        </ScrollView>
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  scrollView: {
+    backgroundColor: "white",
+  },
+  list: {
+    flex: 1
+  },
+  contente: {
+    paddingHorizontal: 20
+  }
 });
+
+export default App;
